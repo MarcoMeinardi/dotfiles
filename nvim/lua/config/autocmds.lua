5a1f7b961a6a20ca3d8f33502c9b7181da305764
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-03-14 09:20:51 +0100

    removed navic

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -15,7 +15,6 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = vim.api.nvim_create_augroup("HighlightYank", {}),
-	pattern = "*",
 	callback = function()
 		vim.highlight.on_yank({
 			higroup = "IncSearch",
