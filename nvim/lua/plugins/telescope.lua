911cd6b535ee8c8b7eaeda50d579a4915fbe1280
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-03-27 20:23:17 +0200

    Telescope prefix selected text

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,18 @@
+local function get_selected_text()
+	local mode = vim.api.nvim_get_mode().mode
+	if mode ~= "v" then
+		return ""
+	end
+
+	local old_reg = vim.fn.getreg("v")
+	vim.cmd('noau normal! "vy')
+	local selected_text = vim.fn.getreg("v")
+	vim.fn.setreg("v", old_reg)
+	selected_text = string.gsub(selected_text, "\n", "")
+
+	return selected_text
+end
+
 return {
 	{
 		"nvim-telescope/telescope.nvim",
@@ -8,27 +23,37 @@ return {
 		keys = {
 			{
 				"<leader>ff",
-				function() require("telescope.builtin").find_files() end,
+				function() require("telescope.builtin").find_files({ default_text = get_selected_text() }) end,
 				mode = { "n", "v" }
 			},
 			{
 				"<leader>fs",
-				function() require("telescope.builtin").live_grep() end,
+				function() require("telescope.builtin").live_grep({ default_text = get_selected_text() }) end,
+				mode = { "n", "v" }
+			},
+			{
+				"<leader>bs",
+				function() require("telescope.builtin").current_buffer_fuzzy_find({ default_text = get_selected_text() }) end,
+				mode = { "n", "v" }
+			},
+			{
+				"<leader>bt",
+				function() require("telescope.builtin").current_buffer_tags({ default_text = get_selected_text() }) end,
 				mode = { "n", "v" }
 			},
 			{
 				"<leader>tg",
-				function() require("telescope.builtin").tags({ ctags_file = ".ctags" }) end,
+				function() require("telescope.builtin").tags({ ctags_file = ".ctags", default_text = get_selected_text() }) end,
 				mode = { "n", "v" }
 			},
 			{
 				"<leader>man",
-				function() require("telescope.builtin").man_pages() end,
+				function() require("telescope.builtin").man_pages({ default_text = get_selected_text() }) end,
 				mode = { "n", "v" }
 			},
 			{
 				"<leader>old",
-				function() require("telescope.builtin").oldfiles() end,
+				function() require("telescope.builtin").oldfiles({ default_text = get_selected_text() }) end,
 				mode = { "n", "v" }
 			}
 		}
