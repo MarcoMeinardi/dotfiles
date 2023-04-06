6a16f92b21996acb8134d4e1f7efb3a49f98f23e
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-04-06 14:20:23 +0200

    Added save compile and run shortcut

--- nvim/ftplugin/c.lua
+++ nvim/ftplugin/c.lua
@@ -1,4 +1,5 @@
 vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
 
 vim.keymap.set("n", "<leader>gcc", "<Cmd>w<CR><Cmd>!gcc -o main % -Wall -Wextra -g3 -fsanitize=signed-integer-overflow -fsanitize=address<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gcr", "<Cmd>w<CR><Cmd>!gcc -o main % -Wall -Wextra -g3 -fsanitize=signed-integer-overflow -fsanitize=address<CR><Cmd>!./main<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>gr", "<Cmd>!./main<CR>", { noremap = true })
