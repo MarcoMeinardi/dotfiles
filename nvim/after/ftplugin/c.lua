4b3d23798a1bcb49787f08f753f487b573645d09
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-04-10 22:53:35 +0200

    [nvim] Moved ftplugin to after for higher priority

--- nvim/after/ftplugin/c.lua
+++ nvim/after/ftplugin/c.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+vim.bo.commentstring = "// %s"
 
 vim.keymap.set("n", "<leader>gcc", "<Cmd>w<CR><Cmd>!gcc -o main % -Wall -Wextra -g3 -fsanitize=signed-integer-overflow -fsanitize=address<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>gcr", "<Cmd>w<CR><Cmd>!gcc -o main % -Wall -Wextra -g3 -fsanitize=signed-integer-overflow -fsanitize=address && ./main<CR>", { noremap = true })
