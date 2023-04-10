97dad8392e9e9847fd07c5de7b85cfa6104ed40b
MarcoMeinardi <marcomeinardi01@gmail.com>
2023-04-10 15:54:36 +0200

    Better compile and run

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
 
 vim.keymap.set("n", "<leader>gpp", "<Cmd>w<CR><Cmd>!g++ -o main % -Wall -Wextra -g3 -std=c++20 -fsanitize=signed-integer-overflow -fsanitize=address<CR>", { noremap = true })
-vim.keymap.set("n", "<leader>gpr", "<Cmd>w<CR><Cmd>!g++ -o main % -Wall -Wextra -g3 -std=c++20 -fsanitize=signed-integer-overflow -fsanitize=address<CR><Cmd>!./main<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>gpr", "<Cmd>w<CR><Cmd>!g++ -o main % -Wall -Wextra -g3 -std=c++20 -fsanitize=signed-integer-overflow -fsanitize=address && ./main<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>gr", "<Cmd>!./main<CR>", { noremap = true })
