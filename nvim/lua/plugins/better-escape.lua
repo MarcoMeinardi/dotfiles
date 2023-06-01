bca71811ea843a4f8027c8fe0b87400c76f106fa
MarcoMeinardi <marco.meinardi@mail.polimi.it>
2023-06-01 17:58:08 +0200

    [nvim] better escape plugin

---
+++ nvim/lua/plugins/better-escape.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"max397574/better-escape.nvim",
+		lazy = false,
+		config = true,
+		opts = {
+			mapping = { "jk", "kj" },
+			timeout = 100,
+			clear_empty_lines = true
+		}
+	}
+}
