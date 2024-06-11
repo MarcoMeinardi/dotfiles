5270466efc0f7d082f870c91a20b021aae2112c7
MarcoMeinardi <marcomeinardi01@gmail.com>
2024-06-11 15:45:26 +0200

    [nvim] Switch from neodev to lazydev

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -4,7 +4,7 @@ return {
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			{ "folke/neodev.nvim", opts = {} },
+			{ "folke/lazydev.nvim" },
 			"mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
 			{
@@ -209,6 +209,22 @@ return {
 			}
 		end
 	},
+
+	  {
+		  "folke/lazydev.nvim",
+		  ft = "lua", -- only load on lua files
+		  opts = {
+			  library = {
+				  -- See the configuration section for more details
+				  -- Load luvit types when the `vim.uv` word is found
+				  { path = "luvit-meta/library", words = { "vim%.uv" } },
+			  },
+		  },
+	  },
+	  {
+		  "Bilal2453/luvit-meta",
+		  lazy = true
+	  },
 
 	-- cmdline tools and lsp servers
 	{
