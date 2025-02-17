467f25f23097f020687efd2426145578941453aa
MarcoMeinardi <marcomeinardi01@gmail.com>
2025-02-17 11:08:12 +0100

    [spwn] Add recvn lambda in template

--- .config/spwn/template.py
+++ .config/spwn/template.py
@@ -8,6 +8,7 @@ context.binary = exe
 ru  = lambda *x, **y: r.recvuntil(*x, **y)
 rl  = lambda *x, **y: r.recvline(*x, **y)
 rc  = lambda *x, **y: r.recv(*x, **y)
+rcn = lambda *x, **y: r.recvn(*x, **y)
 sla = lambda *x, **y: r.sendlineafter(*x, **y)
 sa  = lambda *x, **y: r.sendafter(*x, **y)
 sl  = lambda *x, **y: r.sendline(*x, **y)
@@ -23,6 +24,7 @@ else:
 	r = process(f"{debug_dir}/{{binary_name}}")
 
 {interactions}
+
 
 
 r.interactive()
